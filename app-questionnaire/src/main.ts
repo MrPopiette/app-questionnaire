a51da6360226788012e30cf18219a98ea2ce5231
Maxime MONNIER <maxime.monnier@epsi.fr>
2019-10-14 13:32:55 +0200

    init

--- app-questionnaire/src/main.ts
+++ app-questionnaire/src/main.ts
@@ -4,6 +4,8 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// https://cli.vuejs.org/guide/deployment.html#firebase deployement
+
 new Vue({
   router,
   render: h => h(App)
